df5be4524aad36c39c6d13ced112b4c5631d68ed
Chay <chayangkul_c@cmu.ac.th>
2023-12-26 15:23:16 +0700

    lab12

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <ctime> 
+using namespace std;
+
+int fibonacci(unsigned long int x);
+int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+
+int fibonacci(unsigned long int x){
+   if(x == 0) return 0 ;
+   else if (x == 1)return 1 ;
+   else return fibonacci(x-1) + fibonacci(x-2) ;
+}
